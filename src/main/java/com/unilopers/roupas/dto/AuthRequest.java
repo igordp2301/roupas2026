0a23ae2a714e29602a5afe7ee2e1ab4f584685d4
Felipe Caldeira Akryghti <felipecaldeira2804@gmail.com>
2026-03-05 16:15:58 -0300

    ✨ create AuthRequest and AuthResponse DTOs

---
+++ src/main/java/com/unilopers/roupas/dto/AuthRequest.java
@@ -0,0 +1,18 @@
+package com.unilopers.roupas.dto;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JacksonXmlRootElement(localName = "authRequest")
+public class AuthRequest {
+
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/unilopers/roupas/dto/AuthResponse.java
@@ -0,0 +1,17 @@
+package com.unilopers.roupas.dto;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JacksonXmlRootElement(localName = "authResponse")
+public class AuthResponse {
+
+    private String token;
+}
